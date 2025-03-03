7d0491c8e925cbc0cfd257f3bc4aaa0b4c1c729b
a.vlasov3 <a.vlasov3@g.nsu.ru>
2025-03-03 14:55:03 +0700

    add Main in lab1

---
+++ lab1/main/java/src/Main.java
@@ -0,0 +1,10 @@
+package src;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        BullsAndCows game = new BullsAndCows();
+        game.start();
+    }
+}
